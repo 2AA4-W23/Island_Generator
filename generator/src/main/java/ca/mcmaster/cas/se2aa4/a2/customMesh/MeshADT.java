a9d8e9b6583ebbef0a9c58d177a183fdfcd9ad10
Sarim Zia <54862158+sarzar@users.noreply.github.com>
2023-02-21 00:20:30 -0500

    Started meshADT

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/customMesh/MeshADT.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/customMesh/MeshADT.java
@@ -1,10 +1,22 @@
 package ca.mcmaster.cas.se2aa4.a2.customMesh;
 
 
+import java.util.ArrayList;
 
 public class MeshADT {
 
 
+    public void addAllVertices(ArrayList<customVertex> list){
+
+    }
+
+    public void addAllSegments(ArrayList<CustomSegment> list){
+
+    }
+
+    public void addAllPolygons(ArrayList<customPolygon> list){
+
+    }
 
 
 
