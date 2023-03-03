38eea8873fcde4ee9ac7e2fc7c7cf4a51c5ecbef
hamzahrawasia <hamzah.rawasia@gmail.com>
2023-03-02 22:46:28 -0500

    Attempted the bonus section, by creating an OBJ file

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -4,6 +4,7 @@ import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 
@@ -30,6 +31,31 @@ public class Main {
         Mesh myMesh = generator.generate();
         MeshFactory factory = new MeshFactory();
         factory.write(myMesh, args[0]);
+
+
+        //Created .obj file
+        FileWriter writer = new FileWriter("newobj.obj");
+
+        //added vertices to file using "v"
+        for(int i = 0; i < myMesh.getVerticesCount(); i++){
+            writer.write("v "+ myMesh.getVertices(i).getX()+" "+myMesh.getVertices(i).getY()+"\n");
+        }
+
+        //added segments to file using "l"
+        for(int i = 0; i < myMesh.getSegmentsList().size(); i++){
+            writer.write("l "+ myMesh.getSegments(i).getV1Idx()+" "+myMesh.getSegments(i).getV2Idx()+"\n");
+        }
+
+        //added polygons to file using "f"
+        for(int i =0; i < myMesh.getPolygonsList().size();i++) {
+            writer.write("f ");
+            for (int j = 0; j < myMesh.getPolygons(i).getSegmentIdxsCount(); j++) {
+                writer.write(myMesh.getPolygons(i).getSegmentIdxs(j) + " ");
+            }
+            writer.write("\n");
+        }
+        //closed file
+        writer.close();
     }
 
 }
