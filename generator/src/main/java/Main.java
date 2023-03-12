1c1444573a6deceedeeccee96b8a3ba9f8d2cdde
hamzahrawasia <hamzah.rawasia@gmail.com>
2023-03-12 19:59:02 -0400

    Replaced our generator and visualizer subprojects with the provided template code

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -1,99 +1,24 @@
-import ca.mcmaster.cas.se2aa4.a2.customMesh.CustomMesh;
-import ca.mcmaster.cas.se2aa4.a2.generator.DotGen;
-import ca.mcmaster.cas.se2aa4.a2.generator.IrregularDotGen;
+import ca.mcmaster.cas.se2aa4.a2.generator.adt.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.generator.configuration.Configuration;
+import ca.mcmaster.cas.se2aa4.a2.generator.export.enricher.RandomEnricher;
+import ca.mcmaster.cas.se2aa4.a2.generator.export.Exporter;
+import ca.mcmaster.cas.se2aa4.a2.generator.specification.Buildable;
+import ca.mcmaster.cas.se2aa4.a2.generator.specification.SpecificationFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
-import org.apache.commons.cli.*;
-import java.io.FileWriter;
 import java.io.IOException;
-
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ParseException {
-
-        // Extracting command line parameters
-        Options options = new Options();
-        options.addOption("t", false, "mesh type (grid or irregular)");
-
-        options.addOption("d", false, "debug");
-        options.addOption("p", true, "polygon");
-        options.addOption("r", true, "relaxation");
-        options.addOption("dim", true, "dimension");
-        options.addOption("help", false, "help");
-
-        CommandLineParser cml = new DefaultParser();
-        CommandLine parser = cml.parse(options, args);
-
-        int defDimen = 500;
-        int poly = 25;
-        boolean type = false;
-        int relax = 2;
-        boolean debug = false;
-
-        Mesh myMesh;
-        if(parser.hasOption("t")) {
-            type = true;
-
-            if (parser.hasOption("r")) {
-                relax = Integer.parseInt(parser.getOptionValue("r"));
-            }
-
-            if (parser.hasOption("p")) {
-                poly = Integer.parseInt(parser.getOptionValue("p"));
-            }
-
-            if (parser.hasOption("d")) {
-                debug = Boolean.parseBoolean(parser.getOptionValue("p"));
-            }
+    public static void main(String[] args) throws IOException {
+        Configuration config = new Configuration(args);
+        Buildable specification = SpecificationFactory.create(config);
+        Mesh theMesh = specification.build();
+        Structs.Mesh exported = new Exporter().run(theMesh);
+        if(config.export().containsKey(Configuration.DEMO)) {
+            exported = new RandomEnricher(0.2f).process(exported);
         }
-        else{
-            if (parser.hasOption("dim")) {
-                defDimen = Integer.parseInt(parser.getOptionValue("dim"));
-            }
-        }
-        if (type) {
-            IrregularDotGen irregularGen = new IrregularDotGen(relax, poly,debug);
-            myMesh= irregularGen.irregularGenerate();
-        } else {
-            DotGen generator = new DotGen(debug,defDimen);
-            myMesh = generator.generate();
-        }
-
-        if(parser.hasOption("help")){
-            System.out.println("-t irregular grid" );
-            System.out.println("-p the number of polygons / if you dont use it just uses default values ");
-            System.out.println("-r the number of relaxation / if you dont use it just uses default relaxation");
-            System.out.println("-dim the dimensions(what you want) / if you dont use it just uses default dimensions");
-        }
-
-        MeshFactory factory = new MeshFactory();
-        factory.write(myMesh, args[0]);
-
-
-        //Created .obj file
-        FileWriter writer = new FileWriter("newobj.obj");
-
-        //added vertices to file using "v"
-        for(int i = 0; i < myMesh.getVerticesCount(); i++){
-            writer.write("v "+ myMesh.getVertices(i).getX()+" "+myMesh.getVertices(i).getY()+"\n");
-        }
-
-        //added segments to file using "l"
-        for(int i = 0; i < myMesh.getSegmentsList().size(); i++){
-            writer.write("l "+ myMesh.getSegments(i).getV1Idx()+" "+myMesh.getSegments(i).getV2Idx()+"\n");
-        }
-
-        //added polygons to file using "f"
-        for(int i =0; i < myMesh.getPolygonsList().size();i++) {
-            writer.write("f ");
-            for (int j = 0; j < myMesh.getPolygons(i).getSegmentIdxsCount(); j++) {
-                writer.write(myMesh.getPolygons(i).getSegmentIdxs(j) + " ");
-            }
-            writer.write("\n");
-        }
-        //closed file
-        writer.close();
+        new MeshFactory().write(exported, config.export(Configuration.FILENAME));
     }
 }
--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -1,74 +1,22 @@
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.GraphicRenderer;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.IrregularGraphicRenderer;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.MeshDump;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.SVGCanvas;
-
-import org.apache.commons.cli.*;
-
+import ca.mcmaster.cas.se2aa4.a2.visualizer.configuration.Configuration;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.DebugRenderer;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.GraphicRenderer;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.SVGCanvasProvider;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.Renderer;
 
 import java.awt.*;
-import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ParseException {
-
-        String input = args[0];
-        String output = args[1];
-
-        // Extracting command line parameters
-        boolean debugMode = false;
-
-        Options options = new Options();
-        options.addOption("t", false, "mesh type (grid or irregular)");
-        options.addOption("d", false, "debug");
-        options.addOption("help", false, "help");
-
-
-        CommandLineParser cml = new DefaultParser();
-        CommandLine parser = cml.parse(options, args);
-
-        boolean grid = false;
-
-        // Getting width and height for the canvas
-        Structs.Mesh aMesh = new MeshFactory().read(input);
-        double max_x = Double.MIN_VALUE;
-        double max_y = Double.MIN_VALUE;
-        for (Structs.Vertex v: aMesh.getVerticesList()) {
-            max_x = (Double.compare(max_x, v.getX()) < 0? v.getX(): max_x);
-            max_y = (Double.compare(max_y, v.getY()) < 0? v.getY(): max_y);
-        }
-        // Creating the Canvas to draw the mesh
-        Graphics2D canvas = SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));
-
-
-        Structs.Mesh myMesh;
-
-        if(parser.hasOption("help")){
-            System.out.println("-t irregular mesh, only use it when trying to view an irregular mesh" );
-            System.out.println("-d debugMode");
-        }else {
-            if (parser.hasOption("t")) {
-                grid = true;
-                if (parser.hasOption("d")) {
-                    debugMode = true;
-                }
-            } else {
-                if (parser.hasOption("d")) {
-                    debugMode = true;
-                }
-            }
-
-            GraphicRenderer renderer = new GraphicRenderer();
-            renderer.render(aMesh, canvas, debugMode, grid);
-
-            // Storing the result in an SVG file
-            SVGCanvas.write(canvas, output);
-            // Dump the mesh to stdout
-            MeshDump dumper = new MeshDump();
-            dumper.dump(aMesh);
-        }
+    public static void main(String[] args) throws Exception {
+        Configuration config = new Configuration(args);
+        Structs.Mesh aMesh = new MeshFactory().read(config.input());
+        Graphics2D canvas = SVGCanvasProvider.build(aMesh);
+        Renderer renderer = (config.debug()? new DebugRenderer(): new GraphicRenderer());
+        renderer.render(aMesh, canvas);
+        SVGCanvasProvider.write(canvas, config.output());
     }
+
 }
